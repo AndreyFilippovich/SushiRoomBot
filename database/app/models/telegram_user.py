32bda10a159199e72c5c6588a9a7f643de2a5831
Андрей Филиппович <andrejfilippovic8@gmail.com>
2023-10-04 17:41:54 +0300

    add BigInteger

--- database/app/models/telegram_user.py
+++ database/app/models/telegram_user.py
@@ -1,10 +1,10 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, BigInteger
 
 from app.core.db import Base
 
 
 class TelegramUser(Base):
     name = Column(String(), nullable=False)
-    tg_id = Column(Integer(), nullable=False)
+    tg_id = Column(BigInteger(), nullable=False)
     phone_number = Column(String(), nullable=False)
     birth_day = Column(String(), nullable=False)

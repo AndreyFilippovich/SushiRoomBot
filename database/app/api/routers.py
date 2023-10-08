1d7f3d849ea797db2e91f3149b0ccdd5418cf6af
AleksandrLyo <al.lyo@yandex.ru>
2023-10-08 14:51:03 +0300

    fix

--- database/app/api/routers.py
+++ database/app/api/routers.py
@@ -5,7 +5,7 @@ from app.api.endpoints import telegram_user_router, promotions_router
 main_router = APIRouter()
 
 main_router.include_router(
-    telegram_user_router, prefix='/telegram_user', tags=['Telegram users']
+    telegram_user_router, prefix='/telegram_users', tags=['Telegram users']
 )
 
 main_router.include_router(
